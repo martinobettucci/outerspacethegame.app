b5d299805830e5790d9bb9e5596c08cb44d8fcb3
martinobettucci <MaBe!0301>
2022-02-27 19:05:55 +0100

    Landpage, V1

---
+++ assets/tunes/build-theme.ogg.sh
@@ -0,0 +1,1 @@
+ffmpeg -i bells.wav -i drums.wav -i fx.wav -i guitar.wav -i pads.wav -i piano.wav -i plucks.wav -filter_complex amix=inputs=7:duration=first:dropout_transition=3 -codec:a libvorbis -qscale:a 0 theme.ogg
