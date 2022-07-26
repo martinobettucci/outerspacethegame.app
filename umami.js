0f04c80dccb3ad4d479daadc8603388dc05c5edc
Martino Bettucci <martinobettucci@users.noreply.github.com>
2022-07-26 19:54:45 +0200

    Autoredirect for stolen material

---
+++ umami.js
@@ -0,0 +1,6 @@
+// UNRIGHTFUL USAGE OF LICENCED MATERIAL BEING DETECTED
+// OUR WEBSITE HAVE MODIFIED THE REDIRECT FOR YOUR UNLAWFUL USAGE AND YOUR WEBSITE AND IP HAVE BEEN NOTIFIED TO THE RIGHTFUL OWNER.
+// THIS MESSAGES ARE A WARNING, CEASE THE UNLICENCED USAGE OR ACTIONS WILL BE TAKEN.
+alert('This website have loaded unlicensed material property of outerspacethegame.');
+alert('The unlicensed usage have been notified to the rightful owner.');
+alert('You are requested to cease all use of the infringing material and remove any copy on any unlicenced support including code, styles, images, audio and anything else being not your property BEFORE any action being taken.');
